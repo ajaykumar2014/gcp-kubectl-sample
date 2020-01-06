18beebaced79cb856f6bbe1db7b512e35e98a6ed
ajagupta <akumar298@gmail.com>
2020-01-06 16:06:14 +0530

    express js app

---
+++ gcp-kubectl-expressjs-sample/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    console.log("Welcome to NodeJS Application")
+    res.status(200).json("Welcome to nodejs application");
+})
+app.get('/test',(req,res)=>{
+	res.send('OK');
+});
+
+app.listen(8080,()=>"Server is up and running..");
